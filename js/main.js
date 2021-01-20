168c06d64e04f9d5d925412dca4be405dd0fb4f0
stepan323446 <66179087+stepan323446@users.noreply.github.com>
2021-01-21 01:01:29 +0700

    фигня

--- js/main.js
+++ js/main.js
@@ -6,25 +6,23 @@ $(function()
     $('.info-tabs__menu li').click(function(e)
     {
         e.preventDefault();
-        let obj = e;
-        /*
-        $('.info-tabs__menu li').removeClass('active');
-        $('.info-item_i').removeClass('active');
-        $('.info-tabs__num').removeClass('active');
 
-        $(this).addClass('active');
-        $(this).find('.info-tabs__num').addClass('active');
-        
-        $($(this).children('a').attr('href')).addClass('active'); 
-        */
-       $('.info-tabs__menu li').removeClass('active');
-       $('.info-tabs__num').removeClass('active');
-       
+        if(!$(this).hasClass("active"))
+        {
+            $('.info-tabs__menu li').removeClass('active');
+            $('.info-tabs__num').removeClass('active');
 
-       $(this).addClass('active');
-        $(this).find('.info-tabs__num').addClass('active');
+            $(this).addClass('active');
+            $(this).find('.info-tabs__num').addClass('active');
 
-        $('.info-item_i').slideUp(500);
-        $($(this).children('a').attr('href')).slideDown(500);
+            $('.info-item_i').slideUp(500);
+            $($(this).children('a').attr('href')).slideDown(500);
+        }
+    });
+
+    $('.project-items-inner').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
     });
 });
