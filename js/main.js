71826f4b0ca699076c678b068299af0a978f31a9
stepan323446 <66179087+stepan323446@users.noreply.github.com>
2021-01-21 20:37:32 +0700

    slider

--- js/main.js
+++ js/main.js
@@ -23,6 +23,11 @@ $(function()
     $('.project-items-inner').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 3,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnHocus: true,
+        draggable: false
     });
 });
